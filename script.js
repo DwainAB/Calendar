b9931c45819f561540cf5d40448ffcd10c3091b6
dwain93 <74243765+dwain93@users.noreply.github.com>
2023-04-06 18:19:33 +0200

    ajustage des numéros et des jours

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
 function nbJoursMoisActuel() {
-    const calendar = document.getElementById('calendar')
-    const jours = []
-    const now = new Date();
-    const today = now.getDate()
-    const mois = now.getMonth() + 1; 
-    const annee = now.getFullYear();
-    const nbJours = new Date(annee, mois, 0).getDate();
-    
+  const calendar = document.getElementById('calendar')
+  const calendarHeader = document.getElementById('calendar-header')
+  const joursSemaine = ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam']
+  const now = new Date();
+  const today = now.getDate()
+  const mois = now.getMonth(); 
+  const annee = now.getFullYear();
+  const nbJours = new Date(annee, mois +1, 0).getDate();
   
-    for (let j = 0; j < nbJours; j++) {
-      jours.push(j + 1)
-    }
-  
-    let table7 = []
-  
-    for (let i = 0; i < nbJours; i +=7) {
-     table7.push(jours.slice(i, i + 7))
-    }
+  let jours = []
+  let jourSemaine = new Date(annee, mois +1, today).getDay() // Récupère le jour de la semaine correspondant au premier jour du mois
 
-    console.log(table7);
-  
-    for (let l = 0; l < table7.length; l++) {
-      let row = document.createElement('tr')
-      
-    for (let h = 0; h < table7[l].length; h++) {
-        let cell = document.createElement('td')
-        cell.textContent = table7[l][h]
-        row.appendChild(cell)
-  
-        if( table7[l][h] == today ){
-          cell.classList.add('today')
-        }
-  
-      }
-  
-      calendar.appendChild(row)
-    }
-   
+  let nbLignes = Math.ceil((nbJours + jourSemaine) / 7);
+
+  if (nbLignes < 6 && new Date(annee, mois, nbJours).getDay() === 6) {
+    nbLignes++;
   }
-  
-  nbJoursMoisActuel()
-  
-  let test = document.getElementById('chart')
+  // Créer une ligne d'en-tête pour les jours de la semaine
+  for (let k = 0; k < joursSemaine.length; k++) {
+    let cell = document.createElement('th')
+    cell.textContent = joursSemaine[k]
+    calendarHeader.appendChild(cell)
+  }
 
+  // Créer une ligne pour chaque semaine du mois
+  for (let j = 0; j < nbLignes; j++) {
+    let row = document.createElement('tr')
+
+    // Ajouter des cellules vides pour les jours du mois précédent qui ne font pas partie du mois en cours
+    for (let i = 0; i < jourSemaine; i++) {
+      let cell = document.createElement('td')
+      row.appendChild(cell)
+    }
+
+    // Ajouter une cellule pour chaque jour du mois en cours
+    for (let h = 1; h <= 7 - jourSemaine && jours.length < nbJours; h++) {
+      let cell = document.createElement('td')
+      cell.textContent = jours.length + 1
+      row.appendChild(cell)
+      jours.push(jours.length + 1)
+
+      if (jours.length === today) {
+        cell.classList.add('today')
+      }
+    }
+
+    // Réinitialiser le jour de la semaine à Dimanche pour la prochaine semaine
+    jourSemaine = 0
+
+    calendar.appendChild(row)
+  }
+}
+
+nbJoursMoisActuel()
